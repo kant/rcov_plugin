8451bd26146087d637e0508a77569d13f27e41f9
Alan Johnson <alan@commondream.net>
2010-04-16 22:18:44 -0400

    Getting rid of railtie_name, since it's deprecated.

--- lib/rcov_plugin/railtie.rb
+++ lib/rcov_plugin/railtie.rb
@@ -5,8 +5,6 @@ puts "Loaded rcov_plugin"
 
 module RcovPlugin
   class Railtie < Rails::Railtie
-    railtie_name :rcov_plugin
-
     rake_tasks do
       puts "Loading rake tasks"
       load "tasks/rcov.rake"
